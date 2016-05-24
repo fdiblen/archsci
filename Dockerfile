e6c744fae1744b7899135b966ebc620464e5dde4
Faruk D <f.diblen@esciencecenter.nl>
2016-05-24 12:39:31 +0200

    added antigen and removed powerline

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,14 @@ MAINTAINER fdiblen <f.diblen@esciencecenter.nl>
 
 RUN pacman -Syyu --noconfirm --needed \
     iproute2 net-tools wget \
-    rsync git vim \
-    zsh sudo \
-    python-pip
+    rsync unzip git vim \
+    zsh sudo fontconfig ttf-symbola
 
 RUN rm -rfv /var/cache/pacman/pkg/*
+RUN echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
+    && echo "en_US ISO-8859-1" >> /etc/locale.gen \
+    && locale-gen
+
 
 RUN echo "archsci    ALL=(ALL)    ALL" >> /etc/sudoers.d/archsci
 RUN useradd -ms /bin/zsh -G users,wheel archsci \
@@ -19,7 +22,9 @@ RUN chown archsci:archsci -R /home/archsci
 
 
 COPY zshrc.template /home/archsci/.zshrc
-RUN chown archsci:archsci /home/archsci/.zshrc
+COPY antigen.template /home/archsci/.antigen.archsci
+COPY setShell.bash /home/archsci/setShell.bash
+RUN chown -R archsci:archsci /home/archsci/
 
 
 USER archsci
@@ -27,8 +32,9 @@ ENV HOME /home/archsci
 ENV DISPLAY :0
 
 
-COPY setShell.bash /home/archsci/setShell.bash
-RUN /bin/bash /home/archsci/setShell.bash \
+RUN /bin/zsh /home/archsci/setShell.bash \
     && rm -f /home/archsci/setShell.bash
+
+
 
 CMD ["/bin/zsh"]
--- setShell.bash
+++ setShell.bash
@@ -1,15 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 
-## Oh-my-zsh
-#curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+# Antigen
+echo "Setting antigen"
+echo
 
+git clone https://github.com/zsh-users/antigen.git ${HOME}/.antigen
+echo "source ${HOME}/.antigen/antigen.zsh" >> ${HOME}/.zshrc
+echo "source ${HOME}/.antigen.archsci" >> ${HOME}/.zshrc
 
-#pip install --user git+git://github.com/powerline/powerline
-pip install --user powerline-status
-pip install --user powerline-gitstatus
-pip install --user powerline-docker
+source ${HOME}/.antigen/antigen.zsh
+source ${HOME}/.antigen.archsci
+source ${HOME}/.zshrc
+
 
 # Fonts
+echo "Getting fonts"
+echo
+
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
@@ -19,21 +26,22 @@ mkdir -p ~/.fonts.conf.d/
 mkdir -p ~/.config/fontconfig/conf.d/
 
 mv PowerlineSymbols.otf ~/.fonts/
-fc-cache -vf ~/.fonts/
 
 cp 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.fonts.conf.d/
-
-fc-cache -vf ~/.fonts/
 
 ## Patched fonts
 #mv 'SomeFont for Powerline.otf' ~/.fonts/
 #fc-cache -vf ~/.fonts/
 
+cd && git clone https://github.com/powerline/fonts.git && cd fonts && ./install.sh
+cd && rm -rf fonts
 
-# Own settings
-mkdir -p ~/.config/powerline
+cd && mkdir -p fonts && cd fonts && wget https://cdn.rawgit.com/larsenwork/monoid/138e2bd5a459265522f0471ec7fa5401525b660b/Monoid.zip
+unzip Monoid.zip && mv *.ttf ~/.fonts/
+cd && rm -rf fonts Monoid.zip
 
-git clone https://github.com/jkubrynski/powerline-git-theme.git \
-    && mv powerline-git-theme/powerline ~/.config/ \
-    && rm -rf powerline-git-theme
+fc-cache -vf ~/.fonts/
+fc-cache -vf ~/.fonts.conf.d/
+fc-cache -vf ~/.config/fontconfig/conf.d/
+fc-cache -vf ~/.local/share/fonts
